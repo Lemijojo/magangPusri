46310bd081cb51735036b46546060fd80dcf7006
Lemijojo <l3m1j0j0@gmail.com>
2026-06-30 10:08:31 +0700

    Delete Code

--- runLlm/csvToTxt.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Gunakan jalur lengkap dari /home sampai ke nama filenya
-baca = pd.read_csv("/home/eljo/Pusri/runLlm/data/tokopedia-product-reviews-2019.csv")
-
-# Tentukan juga outputnya mau disimpan di folder yang sama
-baca.to_csv('/home/eljo/Pusri/runLlm/hasil_output.txt', sep='\t', index=False)
-
-print("Konversi CSV ke TXT selesai!")
--- runLlm/runToken.py
+++
@@ -1,7 +0,0 @@
-# Gunakan path lengkap ke tempat hasil_output.txt Anda berada
-dataset = open("/home/eljo/Pusri/runLlm/data/hasil_output.txt", "r", encoding="utf-8").read()
-kandunganKarakter = sorted(set(dataset))
-banyakKarakter = len(kandunganHuruf)
-
-print(kandunganHuruf)
-print(banyakKarakter)
